5ea5dd0abac72134dd1f60670528fdfda9ebc61c
罪无行 <35161724+z915088581@users.noreply.github.com>
2020-01-31 02:28:40 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head> 
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+  <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1.0"> 
+  <meta name="apple-mobile-web-app-capable" content="yes"> 
+  <meta name="apple-mobile-web-app-status-bar-style" content="black"> 
+<title>《自由之战》粉丝官网</title>
+<center>
+<h2>《自由之战》粉丝官网<h3>
+
+
+</center>
+</html>
